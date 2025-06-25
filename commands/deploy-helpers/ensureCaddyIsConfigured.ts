118252c77e810d7ff37bce42ff0aa4b898ae3052
André Ribeiro <andresribeiroo@proton.me>
2025-06-25 19:51:14 -0300

    use caddy from docker

--- commands/deploy-helpers/ensureCaddyIsConfigured.ts
+++ commands/deploy-helpers/ensureCaddyIsConfigured.ts
@@ -1,77 +1,43 @@
 import type { NodeSSH } from "node-ssh";
 import { logCommand, logger } from "../../utils/logger";
 
+const caddyContainerName = "caddy";
+const dockerNetworkName = "ciara-network";
+const remoteCaddyParentFolder = "/root/conf";
+const remoteCaddyfilePath = `${remoteCaddyParentFolder}/Caddyfile`;
+
 export async function ensureCaddyIsConfigured(
 	ssh: NodeSSH,
 	customCaddyfile: string | undefined,
 ) {
 	logger.info(`Ensuring Caddy is configured.`);
-	logger.info("Checking if Caddy is already installed.");
-	const checkCaddyCommand = "which caddy";
-	logCommand(checkCaddyCommand);
-	const checkResult = await ssh.execCommand(checkCaddyCommand);
-	if (checkResult.code === 0) {
-		logger.info("Caddy is already installed. Skipping installation.");
+	logger.info(`Checking if Docker network '${dockerNetworkName}' exists.`);
+	const networkCheckCommand = `docker network inspect ${dockerNetworkName}`;
+	logCommand(networkCheckCommand);
+	const networkCheckResult = await ssh.execCommand(networkCheckCommand);
+	if (networkCheckResult.code !== 0) {
+		logger.info(`Network ${dockerNetworkName} does not exists. Creating it.`);
+		const createNetworkCommand = `docker network create ${dockerNetworkName}`;
+		logCommand(createNetworkCommand);
+		const createNetworkResult = await ssh.execCommand(createNetworkCommand);
+		if (createNetworkResult.code !== 0) {
+			logger.error(
+				`Failed to create Docker network: ${createNetworkResult.stderr}`,
+			);
+			throw new Error("Failed to create Docker network.");
+		}
+		logger.info(`Docker network '${dockerNetworkName}' created.`);
 	} else {
-		logger.info("Caddy not found. Installing Caddy.");
-		logger.info("Installing necessary tools.");
-		const installNecessaryDepsCommand =
-			"sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl";
-		const installNecessaryDepsResult = await ssh.execCommand(
-			installNecessaryDepsCommand,
-		);
-		if (installNecessaryDepsResult.code !== 0) {
-			logger.error(
-				`Failed to install necessary tools: ${installNecessaryDepsResult.stderr}`,
-			);
-			throw new Error("Failed to install necessary tools");
-		}
-		logger.info("Installed Necessary tools.");
-		logger.info("Downloading Caddy public key.");
-		const downloadPublicKeyCommand =
-			"curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg";
-		const downloadPublicKeyResult = await ssh.execCommand(
-			downloadPublicKeyCommand,
-		);
-		if (downloadPublicKeyResult.code !== 0) {
-			logger.error(
-				`Failed to download Caddy public key: ${downloadPublicKeyResult.stderr}`,
-			);
-			throw new Error("Failed to download Caddy public key.");
-		}
-		logger.info("Downloaded Caddy public key.");
-		logger.info("Adding Caddy repository.");
-		const addCaddyRepositoryCommand =
-			"curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list";
-		const addCaddyRepositoryResult = await ssh.execCommand(
-			addCaddyRepositoryCommand,
-		);
-		if (addCaddyRepositoryResult.code !== 0) {
-			logger.error(
-				`Failed to add Caddy repository: ${addCaddyRepositoryResult.stderr}`,
-			);
-			throw new Error("Failed to add Caddy repository.");
-		}
-		logger.info("Caddy repository added.");
-		logger.info("Installing Caddy.");
-		const installCaddyCommand =
-			"sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install -y caddy";
-		const installCaddyResult = await ssh.execCommand(installCaddyCommand);
-		if (installCaddyResult.code !== 0) {
-			logger.error(`Failed to install Caddy: ${installCaddyResult.stderr}`);
-			throw new Error("Failed to install Caddy.");
-		}
-		logger.info("Caddy installed successfully.");
+		logger.info(`Docker network '${dockerNetworkName}' already exists.`);
 	}
-	const remoteCaddyfilePath = "/etc/caddy/Caddyfile";
 	logger.info(`Copying Caddyfile to ${remoteCaddyfilePath}.`);
 	const caddyfileContent = customCaddyfile
 		? await Bun.file(customCaddyfile).text()
 		: `
-	  :2024
-    respond "Hello from Caddy!"
+	  :80, :443
+    respond "Hey from Caddy!"
 	`;
-	const copyCaddyfileCommand = `echo '${caddyfileContent}' | sudo tee ${remoteCaddyfilePath} && caddy fmt --overwrite ${remoteCaddyfilePath}`;
+	const copyCaddyfileCommand = `sudo mkdir -p ${remoteCaddyParentFolder} && echo '${caddyfileContent}' | sudo tee ${remoteCaddyfilePath}`;
 	logCommand(copyCaddyfileCommand);
 	const copyResult = await ssh.execCommand(copyCaddyfileCommand);
 	if (copyResult.stderr) {
@@ -79,88 +45,45 @@ export async function ensureCaddyIsConfigured(
 		throw new Error("Failed to copy Caddyfile.");
 	}
 	logger.info("Caddyfile copied.");
-	logger.info("Checking if Caddy service is running.");
-	const checkCaddyStatus = "systemctl is-active caddy";
-	logCommand(checkCaddyStatus);
-	const caddyStatusResult = await ssh.execCommand(checkCaddyStatus);
-	const isCaddyAlreadyRunning = caddyStatusResult.stdout === "active";
-	if (isCaddyAlreadyRunning) {
-		logger.info("Caddy is already running.");
-		logger.info("Reloading Caddy configuration.");
-		const reloadCommand = `caddy reload --config ${remoteCaddyfilePath} --adapter caddyfile`;
+	logger.info("Checking if Caddy container is running.");
+	const checkCaddyCommand = `docker ps -f name=${caddyContainerName} --format "{{.Names}}"`;
+	logCommand(checkCaddyCommand);
+	const checkResult = await ssh.execCommand(checkCaddyCommand);
+	const isCaddyRunning = checkResult.stdout.trim() === caddyContainerName;
+	if (isCaddyRunning) {
+		logger.info("Caddy is already running. Reloading configuration.");
+		const reloadCommand = `docker exec -w /etc/caddy ${caddyContainerName} caddy reload`;
 		logCommand(reloadCommand);
 		const reloadResult = await ssh.execCommand(reloadCommand);
-		if (
-			!reloadResult.stderr.includes("adapted") &&
-			!reloadResult.stdout.includes("adapted")
-		) {
+		if (reloadResult.code !== 0) {
 			logger.error(`Error reloading Caddy: ${reloadResult.stderr}`);
 			throw new Error("Error reloading Caddy.");
 		}
 		logger.info("Caddy configuration reloaded.");
 	} else {
-		logger.info("Caddy is not already running.");
-		logger.info("Starting Caddy service.");
-		const startCaddyService =
-			"sudo systemctl daemon-reload && sudo systemctl enable --now caddy";
-		logCommand(startCaddyService);
-		const startCaddyResult = await ssh.execCommand(startCaddyService);
-		if (startCaddyResult.stderr) {
-			logger.error(`Failed to start Caddy: ${startCaddyResult.stderr}`);
-			throw new Error(`Failed to start Caddy service.`);
+		logger.info("Caddy is not running.");
+		logger.info("Removing old containers.");
+		const removeOldContainerCommand = `docker rm -f ${caddyContainerName}`;
+		logCommand(removeOldContainerCommand);
+		await ssh.execCommand(removeOldContainerCommand); // We don't care if it fails (e.g., if it doesn't exist)
+		logger.info("Old containers removed.");
+		logger.info("Pulling latest Caddy image.");
+		const pullCommand = "docker pull caddy:latest";
+		logCommand(pullCommand);
+		const pullResult = await ssh.execCommand(pullCommand);
+		if (pullResult.code !== 0) {
+			logger.error(`Failed to pull Caddy image: ${pullResult.stderr}`);
+			throw new Error("Failed to pull Caddy image.");
 		}
-		logger.info("Caddy service started.");
-		logger.info("Checking Caddy service status.");
-		const checkCaddyServiceStatus = "systemctl status caddy";
-		logCommand(checkCaddyServiceStatus);
-		const statusResult = await ssh.execCommand(checkCaddyServiceStatus);
-		if (!statusResult.stdout.includes("active (running)")) {
-			logger.error(`Caddy service is not running.: ${statusResult.stdout}`);
-			throw new Error(`Caddy service is not running.`);
+		logger.info("Starting Caddy container.");
+		const startCaddyContainerCommand = `docker run -d --name ${caddyContainerName} --network ${dockerNetworkName} -p 80:80 -p 443:443 -p 443:443/udp -v ${remoteCaddyParentFolder}:/etc/caddy -v caddy_data:/data -v caddy_config:/config caddy:latest`;
+		logCommand(startCaddyContainerCommand);
+		const startResult = await ssh.execCommand(startCaddyContainerCommand);
+		if (startResult.code !== 0) {
+			logger.error(`Failed to start Caddy container: ${startResult.stderr}`);
+			throw new Error("Failed to start Caddy container.");
 		}
-		logger.info("Caddy service is active and running.");
-		logger.info("Checking if Caddy is configured to start on boot.");
-		const caddyServiceFilePath = "/etc/systemd/system/caddy.service";
-		const checkServiceFileCommand = `sudo test -f ${caddyServiceFilePath}`;
-		logCommand(checkServiceFileCommand);
-		const checkServiceFileResult = await ssh.execCommand(
-			checkServiceFileCommand,
-		);
-		if (checkServiceFileResult.stderr) {
-			logger.info("Caddy is not configured no start on boot.");
-			logger.info("Configuring Caddy to start on boot.");
-			logger.info("Creating Caddy systemd service file.");
-			const serviceFileContent = `
-        Description=Caddy
-        Documentation=https://caddyserver.com/docs/
-        After=network.target
-
-        [Service]
-        ExecStart=/usr/bin/caddy run --environ --config /etc/caddy/Caddyfile
-        ExecReload=/usr/bin/caddy reload --config /etc/caddy/Caddyfile
-        TimeoutStopSec=5s
-        LimitNOFILE=1048576
-        LimitNPROC=512
-        PrivateTmp=true
-        ProtectSystem=full
-        AmbientCapabilities=CAP_NET_BIND_SERVICE
-
-        [Install]
-        WantedBy=multi-user.target
-      `;
-			const createServiceFileCommand = `echo "${serviceFileContent.trim()}" | sudo tee ${caddyServiceFilePath}`;
-			logCommand(createServiceFileCommand);
-			const createServiceFileResult = await ssh.execCommand(
-				createServiceFileCommand,
-			);
-			if (createServiceFileResult.stderr) {
-				logger.error(
-					`Failed to create Caddy service file: ${createServiceFileResult.stderr}`,
-				);
-				throw new Error("Failed to create Caddy systemd service file.");
-			}
-			logger.info("Caddy systemd service file created successfully.");
-			logger.info("Caddy is now configured to start on boot.");
-		}
+		logger.info("Caddy container started.");
 	}
+	logger.info("Caddy is up and running.");
 }
