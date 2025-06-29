b8dfedc07e1509e5b43749a5fdf9bc8119d058f9
André Ribeiro <andresribeiroo@proton.me>
2025-06-29 15:54:06 -0300

    fix deploy command

--- commands/deploy-helpers/readCiaraConfig.ts
+++ commands/deploy-helpers/readCiaraConfig.ts
@@ -1,10 +1,10 @@
-import path from "node:path";
 import { type } from "arktype";
+import { getConfigPath } from "../../utils/getConfigPath";
 import { logger } from "../../utils/logger";
 import { CiaraConfig } from "../validate";
 
 export async function readCiaraConfig() {
-	const configPath = path.join("/workdir", "ciara.config.json");
+	const configPath = getConfigPath();
 	const file = Bun.file(configPath);
 	const exists = await file.exists();
 	if (!exists) {
